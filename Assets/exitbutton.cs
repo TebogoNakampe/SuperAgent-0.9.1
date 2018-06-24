20d6d60bfb3219b128a1cb31fffd893cf1a40b98
ABInt Xerus <molnakampe@gmail.com>
2018-06-24 16:22:59 +0200

    Add files via upload

---
+++ Assets/exitbutton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class exitbutton : MonoBehaviour {
+
+    /* Use this for initialization
+	void Start () {
+		
+	}*/
+    public void ClickExit()
+
+    {
+        Application.Quit();
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
